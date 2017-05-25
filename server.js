80849f16c94e5bcb52899e43c40b51892e4b25c4
Brandon Wissmann <brandonwissmann@hotmail.com>
2017-05-24 21:39:26 -0400

    finished

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-var express = request("express");
+var express = require("express");
+var path = require("path");
 var app = express();
 
+app.use(express.static('public'));
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
 function onHttpStart() {
-    console.log("Express http server listening on: " + HTTP_PORT);
+  console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.get("/about", function(req, res){
-    res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+   res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-   res.send("<h3>About</h3>");
+  res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
 // setup http server to listen on HTTP_PORT
