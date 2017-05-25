e1239d25b978d971151cc6c192673552290ca5de
Brandon Wissmann <brandonwissmann@hotmail.com>
2017-05-24 21:47:44 -0400

    updates

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+* WEB322 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+* of this assignment has been copied manually or electronically from any other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Brandon Wissmann Student ID: 122538168 Date: 5/24/17
+*
+* Online (Heroku) Link: ________________________________________________________
+*
+********************************************************************************/
+
 var express = require("express");
 var path = require("path");
 var app = express();
@@ -19,6 +31,10 @@ app.get("/", function(req,res){
 // setup another route to listen on /about
 app.get("/about", function(req,res){
   res.sendFile(path.join(__dirname + "/views/about.html"));
+});
+
+app.get("/scramble", function(req,res){
+  res.sendFile(path.join(__dirname + "/node_modules/scramblejs/dist/scramble.js"));
 });
 
 // setup http server to listen on HTTP_PORT
