30bd17bed91abd22701f67cf1c99bf49863995d4
Brandon Wissmann <brandonwissmann@hotmail.com>
2017-06-19 11:58:14 -0400

    adding handlebars

--- server.js
+++ server.js
@@ -13,9 +13,30 @@
 const express = require("express");
 const dataService = require("./data-service.js");
 const path = require("path");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine(".hbs", exphbs({
+    extname: ".hbs",
+    defaultLayout: "layout",
+    helpers: {
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) 
+                return options.inverse(this);
+            else 
+            return options.fn(this);
+        }
+    }
+}));
+
+app.set("view engine", ".hbs");
 
 var HTTP_PORT = process.env.PORT || 8080;
 
